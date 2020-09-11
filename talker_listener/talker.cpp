b39d8d86b6ec0f4185f6abced8af30028d50beaa
“Midhun <midhun@umd.edu>
2020-09-11 00:47:54 -0400

    Formatted the talker.cpp code to incorporate proper comments

--- talker_listener/talker.cpp
+++ talker_listener/talker.cpp
@@ -1,41 +1,54 @@
 #include "ros/ros.h"  /* To include the ROS header file*/
-#include "std_msgs/String.h"  /* Header file for standard*/ 
+#include "std_msgs/String.h"  /* Header file for to include the std_msgs from the package*/ 
 #include <sstream> 
 
 
 int main(int argc, char **argv)
 {
   /*
-   This is the main function for the ROS package/code and it needs two arguments as given above
+   This is the main function for the ROS package/code and it needs two arguments as given above.
+   This initializes ROS and here we specify the name of the node.
    */
   ros::init(argc, argv, "talker");
 
   /**
-   * NodeHandle is the main access point to communications with the ROS system.
+   This section creates a nodehandle for the node
+   NodeHandle is the main access point to communications with the ROS system.
   */
   ros::NodeHandle n;
 
   /**
+   Tell the master that we are going to be publishing a message of type std_msgs/String on the topic chatter
    The advertis function receives two arguments, first the message and the second the size of the message
    */
   ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
 
+  /**
+   Allows to specify the frequency at which to loop
+   */
   ros::Rate loop_rate(10);
 
+  /**
+   Allows to specify the frequency at which to loop
+   */
   int count = 0;
   while (ros::ok())
   {
     /**
-     * This is a message object. You stuff it with data, and then publish it.
+     By default roscpp will install a SIGINT handler which provides Ctrl-C 
+     handling which will cause ros::ok() to return false if that happens. 
      */
     std_msgs::String msg;
 
     std::stringstream ss;
     ss << "This is my first ROS node" << count;
     msg.data = ss.str();
-
+       
     ROS_INFO("%s", msg.data.c_str());
 
+    /*
+     Broadcast the message to anyone who is connected 
+    */
     chatter_pub.publish(msg);
 
     ros::spinOnce();
