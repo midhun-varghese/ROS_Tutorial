58abdb5eb3be6ce1b312610c5c33a73ce2c18bd8
“Midhun <midhun@umd.edu>
2020-09-12 01:40:16 -0400

    Formatted the talker node in Python

--- talker_listener/src/talker.py
+++ talker_listener/src/talker.py
@@ -2,11 +2,11 @@ import rospy    # Main header for ROS
 from std_msgs.msg import String    # From the std_msgs class import the String
 
 def talker(): # After the main function is executed this talker function will be activated
-    pub = rospy.Publisher('chatter', String, queue_size=10)
+    pub = rospy.Publisher('chatter', String, queue_size=10) # Pusblishes the chatter topic of type String
     rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
-    while not rospy.is_shutdown():
-        hello_str = "hello world %s" % rospy.get_time()
+    rate = rospy.Rate(10) # Defines the rate of communication 
+    while not rospy.is_shutdown():  # Check to see if CTRL+C is pressed
+        hello_str = "My first ROS code in Python %s" % rospy.get_time()
         rospy.loginfo(hello_str)
         pub.publish(hello_str)
         rate.sleep()
